cba66a115b5974e4cbd72def7399a442f48b169d
patricklafrance <pa.lafrance@gmail.com>
2023-01-16 23:21:09 -0500

    Updateds docs

--- packages/static-module-1/src/register.tsx
+++ packages/static-module-1/src/register.tsx
@@ -44,7 +44,7 @@ export const register: ModuleRegisterFunction = (runtime: Runtime) => {
             content: (
                 <>
                     <ArchiveIcon />
-                    <span>Static1/Page 1 - Navigation item with a React element as content and additional Link props</span>
+                    <span>Static1/Page 1 - Item with a React element as content + additional Link props</span>
                 </>
             ),
             style: {
@@ -55,21 +55,21 @@ export const register: ModuleRegisterFunction = (runtime: Runtime) => {
         },
         {
             to: "static1/page-2",
-            content: "Static1/Page 2 - Navigation item with children",
+            content: "Static1/Page 2 - Item with children",
             children: [
                 {
                     to: "static1/page-4",
-                    content: "Static1/Page 4 - Child navigation item"
+                    content: "Static1/Page 4 - Child item"
                 },
                 {
                     to: "static1/page-5",
-                    content: "Static1/Page 5 - Child navigation item"
+                    content: "Static1/Page 5 - Child item"
                 }
             ]
         },
         {
             to: "static1/page-3",
-            content: "Static1/Page 3 - Navigation item with a top priority and custom additional props",
+            content: "Static1/Page 3 - Item with a top priority and custom additional props",
             priority: 99,
             additionalProps: {
                 highlight: true
